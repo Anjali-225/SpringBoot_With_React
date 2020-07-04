c3a96dc4ef8919d7232a0d9de50b16a737d76adf
Anjali <anjali.payal69@gmail.com>
2020-07-04 14:57:56 +0200

    Playing with react class Components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      My Hello World!
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       My Hello World!
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        My Hello World
+        <FirstComponent />
+        <SecondComponent />
+      </div>
+    );
+  }
+}
+
+//Class Component
+class FirstComponent extends Component {
+  render() {
+    return (
+      <div className="firstComponent">
+        FirstComponent
+      </div>
+    );
+  }
+}
+
+class SecondComponent extends Component {
+  render() {
+    return (
+      <div className="SecondComponent">
+        SecondComponent
+      </div>
+    );
+  }
 }
 
 export default App;
