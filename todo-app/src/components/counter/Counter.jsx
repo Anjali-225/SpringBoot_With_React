804ae9b36f7aaef9798ba0296593ea18fadabcf5
Anjali <anjali.payal69@gmail.com>
2020-07-04 18:40:32 +0200

    Increment Button

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import './Counter.css';
+import { render } from '@testing-library/react';
 
-function Counter() {
-    return (
-        <div className="counter">
-            <button>+1</button>
-            <span className="count" > 0 </span>
-        </div>
-    );
+class Counter extends Component {
+
+    //Define the initial state in a constructor
+    //state => counter 0
+    constructor() {
+        super() //Error 1
+        this.state = {
+            counter : 0
+        }
+
+        this.increment = this.increment.bind(this);
+    }
+
+
+
+    render() {
+        return (
+            <div className="counter">
+                <button onClick={this.increment}>+1</button>
+                <span className="count" > {this.state.counter} </span>
+            </div>
+        );
+    }
+
+    increment() { //Update state - counter++
+        //console.log('increment');
+        // this.state.counter++
+        this.setState({
+            counter: this.state.counter + 1 
+        })
+    }
+    
 }
+
+
 
 export default Counter;
