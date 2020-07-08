4d77e9e3ba012975c51a47dbac1d04353c7f6d31
Anjali <anjali.payal69@gmail.com>
2020-07-08 12:59:22 +0200

    End Of Counter App

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -29,7 +29,6 @@ class Counter extends Component {
     reset() {
         this.setState( {counter: 0} ) ;
     }
-
      
     increment(by){
         //console.log(`increment from child - ${by}`);
@@ -57,23 +56,22 @@ class CounterButton extends Component {
     //state => counter 0
     constructor() {
         super() //Error 1
-        this.state = {
-            counter : 0,
-        }
+       
+        // this.state = {
+        //     counter : 0,
+        // }
 
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
+        // this.increment = this.increment.bind(this);
+        // this.decrement = this.decrement.bind(this);
     }
-
-
 
     //render = () => {
     render() {
         return (
             <div className="counter">
-                <button onClick={this.increment}>+{this.props.by}</button>
+                <button onClick={() => this.props.incrementMethod(this.props.by)} >+{this.props.by}</button>
 
-                <button onClick={this.decrement}>-{this.props.by}</button>
+                <button onClick={() => this.props.decrementMethod(this.props.by)} >-{this.props.by}</button>
 
                 {/*<span className="count" > {this.state.counter} </span>*/}
 
@@ -82,29 +80,29 @@ class CounterButton extends Component {
     }
 
     
-    //increment = () => { //Update state - counter++
-    increment(){
-        //console.log('increment');
-        // this.state.counter++  //this is very bad practice 
-        this.setState(
-            (prevState) => {
-                return {counter: prevState.counter + this.props.by}    
-            }
-        );
+    // //increment = () => { //Update state - counter++
+    // increment(){
+    //     //console.log('increment');
+    //     // this.state.counter++  //this is very bad practice 
+    //     this.setState(
+    //         (prevState) => {
+    //             return {counter: prevState.counter + this.props.by}    
+    //         }
+    //     );
 
-        this.props.incrementMethod(this.props.by);
-    }
+    //     this.props.incrementMethod(this.props.by);
+    // }
 
     
-    decrement () {        
-        this.setState(
-            (prevState) => {
-                return {counter: prevState.counter - this.props.by}    
-            }
-        );
+    // decrement () {        
+    //     this.setState(
+    //         (prevState) => {
+    //             return {counter: prevState.counter - this.props.by}    
+    //         }
+    //     );
 
-        this.props.decrementMethod(this.props.by);
-    }
+    //     this.props.decrementMethod(this.props.by);
+    // }
     
 }
 
@@ -115,7 +113,5 @@ CounterButton.defaultProps = {
 CounterButton.PropsTypes = {
     by  : PropsTypes.number
 }
-
-
 
 export default Counter;
