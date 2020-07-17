015a757edcd0447c38745c7541dcbeb33bab9090
Anjali <anjali.payal69@gmail.com>
2020-07-17 10:58:23 +0200

    Enhancing Spring Method

---
+++ restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,29 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+	
+	private String message;
+
+	//Constructor method
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+			return message;
+		}
+	
+	@Override
+	public String toString() {
+//		return "HelloWorldBean [message=" + message + "]";
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+
+	
+	
+
+}
