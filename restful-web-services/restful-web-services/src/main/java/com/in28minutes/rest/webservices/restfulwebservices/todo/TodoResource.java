636fecbe8f2c199b8fc7454e18350af1c0c5d995
Anjali <anjali.payal69@gmail.com>
2020-07-20 19:21:32 +0200

    Understanding Lifecycle Methods

--- restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,7 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,17 @@ public class TodoResource {
 	public List<Todo> getAllTodos(@PathVariable String username) {
 		return todoService.findAll();
 		     
+	}
+	
+	//DELETE /users/{username}/todo/{id}
+	@DeleteMapping("/users/{username}/todo/{id}")
+	public ResponseEntity<Void> deleteTodo(
+			@PathVariable String username, @PathVariable long id){
+		
+		Todo todo = todoService.deleteById(id);
+		if(todo != null) {
+			return ResponseEntity.noContent().build();			
+		}
+		return ResponseEntity.notFound().build();
 	}
 }
