19739706dba523cb277277bbdaebbe7b4aee0099
Anjali <anjali.payal69@gmail.com>
2020-07-23 16:36:56 +0200

    PUT Request Method

--- restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,11 +1,14 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.in28minutes.rest.webservices.restfulwebservices.todo.Todo;
 import java.util.List;
@@ -40,4 +43,24 @@ public class TodoResource {
 		}
 		return ResponseEntity.notFound().build();
 	}
+	
+	//Edit/Update a Todo
+	//PUT /users//{user_name}/todos/{todo_id}
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String username,
+			@PathVariable long id,
+			@RequestBody Todo todo){
+		
+		Todo todoUpdated = todoService.save(todo);
+		
+		return new ResponseEntity<>(todo, HttpStatus.OK);
+	}
+	
+	
+	
+	//Create a new Todo
+	//POST /users/{user_name}/todos/
+	
+	
 }
