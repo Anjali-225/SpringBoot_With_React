cdbb0b83d0eca168d858ae417398608da1f8b83e
Anjali <anjali.payal69@gmail.com>
2020-07-30 15:21:26 +0200

    Enhance React Frontend

--- todo-app/src/components/todo/AuthenticationService.js
+++ todo-app/src/components/todo/AuthenticationService.js
@@ -4,13 +4,22 @@ import axios from 'axios';
 
 class AuthenticationService {
 
+    executeBasicAuthenticationService(username, password){
+        // let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
+        return axios.get('http://localhost:8080/basicauth', 
+            {headers: {authorization: this.createBasicAuthToken(username, password)}})
+    }
+
+    createBasicAuthToken(username, password) {
+        return 'Basic ' + window.btoa(username + ":" + password);         
+    }
+
     registerSuccessfulLogin(username, password) {
+        // let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
 
-        let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
-
-        console.log("registerSuccessfulLogin")
+        //console.log("registerSuccessfulLogin")
         sessionStorage.setItem('authenticatedUser', username);
-        this.setupAxiosInterceptors(basicAuthHeader);
+        this.setupAxiosInterceptors( this.createBasicAuthToken(username, password));
     }
 
     logout() {
