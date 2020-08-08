cacf987acd1eeca12597ffd06166939cda9b9d55
Anjali <anjali.payal69@gmail.com>
2020-08-08 19:17:11 +0200

    More JPA Repository

--- jpa-in-10-steps/jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
+++ jpa-in-10-steps/jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
@@ -1,5 +1,8 @@
 package com.in28minutes.learning.jpa.jpain10steps;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +24,15 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		User user = new User("Jill", "Admin");
-		userRepository.save(user);
+		userRepository.save(user);		
 		log.info("New user is created: " + user);
+		
+		Optional<User> userWithIdOne = userRepository.findById(1L);
+		log.info("User is retrieved: " + userWithIdOne);
+		
+		List<User> users = userRepository.findAll();
+		log.info("All Users: " + users);
+			
 		
 	}
 
